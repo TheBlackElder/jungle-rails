d7d3f4a453b689a87fc9d16c9f0069962b864b8c
TheBlackElder <freedxcollective@gmail.com>
2023-03-14 14:34:07 -0400

    create test for product details

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -3,6 +3,7 @@ describe("Visiting the homepage", () => {
   it("should visit the homepage after logging in", () => {
     // visit the login page
     cy.visit("http://localhost:3000", { failOnStatusCode: false });
+
   });
 
   it("There is products on the page", () => {
